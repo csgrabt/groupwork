7fe869561f4fd91d0ef2f3523b2218c6d4c500d6
exylaci <exy@freemail.hu>
2021-06-09 17:08:27 +0200

    MovieService

---
+++ src/main/java/movie/MovieService.java
@@ -0,0 +1,10 @@
+package movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieService {
+    private List<Movie> movies = new ArrayList<>();
+
+    
+}
---
+++ src/test/java/movie/MovieServiceTest.java
@@ -0,0 +1,7 @@
+package movie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+}
