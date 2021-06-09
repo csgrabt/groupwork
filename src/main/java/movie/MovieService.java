6173c2f7c26591b3f56c521d59486d96ccbcf47e
exylaci <exy@freemail.hu>
2021-06-09 17:16:57 +0200

    MovieService v1.béta

--- src/main/java/movie/MovieService.java
+++ src/main/java/movie/MovieService.java
@@ -1,10 +1,28 @@
 package movie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MovieService {
     private List<Movie> movies = new ArrayList<>();
 
-    
+    public void save(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> findMovieByNamePart(String part) {
+        return movies
+                .stream()
+                .filter(movie -> movie.getName().contains(part))
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Movie> findNewest() {
+        return movies
+                .stream()
+                .max(Comparator.comparing(Movie::getReleaseDate));
+    }
 }
