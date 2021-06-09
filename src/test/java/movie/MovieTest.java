d0063635099b559eb8ed544d66ca85e076a39a73
Csekő György <gy.cseko@outlook.com>
2021-06-09 17:03:25 +0200

    üres tesztesetek

---
+++ src/test/java/movie/MovieTest.java
@@ -0,0 +1,32 @@
+package movie;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void setLength() {
+    }
+
+    @Test
+    void getReleaseDate() {
+    }
+
+    @Test
+    void setReleaseDate() {
+    }
+}
